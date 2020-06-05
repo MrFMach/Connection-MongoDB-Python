1cbc4fed83bbca191836d34f8b944adba88b9850
MrFMach <fabiosilvamachado@gmail.com>
2020-06-05 15:40:56 -0300

    filtrando resultados e exemplo de update

--- Connection-MongoDB-Python.py
+++ Connection-MongoDB-Python.py
@@ -1,5 +1,6 @@
 #importar classe MongoClient da biblioteca pymongo
 from pymongo import MongoClient
+import pprint
 
 cliente = MongoClient('mongodb://localhost:27017')
 
@@ -7,31 +8,38 @@ cliente = MongoClient('mongodb://localhost:27017')
 banco = cliente ['meubanco']
 #criando coleção 'meuscursos' no mongodb e atribuindo à variável local cursos
 cursos = banco ['meuscursos']
-#inserindo vários documentos
+#inserindo vários documentos em forma de matriz
+
 matriz_cursos = [
     {
-        'autor': 'José da Silva',
-        'curso': 'Python avançado',
-        'valor': 130,
-        'classificação': 5 
+        'autor': 'Higor',
+        'curso': 'Linguagem C',
+        'valor': 290,
+        'classificação': 4
     },  {
-        'autor': 'Luan Cardoso',
-        'curso': 'Lógica de Programação',
-        'valor': 70,
-        'classificação': 4 
-    },  {
-        'autor': 'Rita Nogueira',
-        'curso': 'HTML e CSS Básico',
-        'valor': 180,
-        'classificação': 4 
-    },  {
-        'autor': 'Mai Tsnuga',
-        'curso': 'Algorítmos',
-        'valor': 135,
-        'classificação': 3 
+        'autor': 'Teo',
+        'curso': 'POO',
+        'valor': 79,
+        'classificação': 3
     },
 ]
 
+#inserindo a matriz na coleção e imprimindo mensagem dos cursos inseridos
 resultado = cursos.insert_many(matriz_cursos)
 for objeto_id in resultado.inserted_ids:
     print('Curso inserido com sucesso. O ID do curso é: ' + str(objeto_id))
+
+#encontrando resultados específicos, ordenados e limitados
+resultado = cursos.find().sort([('autor', 1), ('valor', -1)]).limit(3)
+#imprimindo resultados utilizando pprint (várias linhas)
+for res in resultado:
+    pprint.pprint(res)
+
+#Update
+cursos.update_one({
+    'curso': 'MongoDB'
+}, {
+   '$set': {
+       'curso': 'MongoDB - Fundamentos'
+   } 
+})
