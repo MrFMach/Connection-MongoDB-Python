cc5da4bfce1d3925a342a38aafc67f3fccfa5c4b
MrFMach <fabiosilvamachado@gmail.com>
2020-06-05 13:49:54 -0300

    criando um documento

--- Connection-MongoDB-Python.py
+++ Connection-MongoDB-Python.py
@@ -3,10 +3,22 @@ from pymongo import MongoClient
 
 cliente = MongoClient('mongodb://localhost:27017')
 
-#criando banco 'meu banco' no mongodb e atribuindo à variável banco
+#criando banco 'meubanco' no mongodb e atribuindo à variável local banco
 banco = cliente ['meubanco']
-#criando coleção 'cursos' e atribuindo à variável meuscursos
+#criando coleção 'meuscursos' no mongodb e atribuindo à variável local cursos
 cursos = banco ['meuscursos']
+#criando um documento chamado curso
+curso = {
+    'autor':'Maria',
+    'curso':'MongoDB',
+    'valor':120,
+    'classificação':5
+}
+#inserindo o documento curso na coleção meuscursos
+resultado = cursos.insert_one(curso)
+
+if resultado.acknowledged:
+    print('Curso inserido com sucesso. O ID do documento é ' + str(resultado.inserted_id))
 
 
-print(cursos)
+
